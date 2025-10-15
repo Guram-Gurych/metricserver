7090eeabd0de6975e04a4856050f4c914b46983e
Blednykh Kirill <blednyhkirill@yandex.ru>
2025-10-15 20:56:58 +0300

    fix

--- internal/config/config.go
+++ internal/config/config.go
@@ -14,22 +14,31 @@ type Config struct {
 
 func InitConfigServer() *Config {
 	var config Config
+	var reportIntervalSec, pollIntervalSec int64
 
 	flag.StringVar(&config.ServerAddress, "a", "localhost:8080", "Адрес для запуска HTTP-сервера")
-	flag.DurationVar(&config.ReportInterval, "r", 10*time.Second, "Частота отправки метрик на сервер")
-	flag.DurationVar(&config.PollInterval, "p", 2*time.Second, "Частота опроса метрик")
+	flag.Int64Var(&reportIntervalSec, "r", 10, "Частота отправки метрик на сервер (в секундах)")
+	flag.Int64Var(&pollIntervalSec, "p", 2, "Частота опроса метрик (в секундах)")
 
 	flag.Parse()
+
+	config.ReportInterval = time.Duration(reportIntervalSec) * time.Second
+	config.PollInterval = time.Duration(pollIntervalSec) * time.Second
 
 	return &config
 }
 
 func InitConfigAgent() *Config {
 	var config Config
+	var reportIntervalSec, pollIntervalSec int64
+
 	flag.StringVar(&config.ServerAddress, "a", "localhost:8080", "Адрес эндпоинта HTTP-сервера")
-	flag.DurationVar(&config.ReportInterval, "r", 10*time.Second, "Частота отправки метрик на сервер")
-	flag.DurationVar(&config.PollInterval, "p", 2*time.Second, "Частота опроса метрик")
+	flag.Int64Var(&reportIntervalSec, "r", 10, "Частота отправки метрик на сервер (в секундах)")
+	flag.Int64Var(&pollIntervalSec, "p", 2, "Частота опроса метрик (в секундах)")
 	flag.Parse()
+
+	config.ReportInterval = time.Duration(reportIntervalSec) * time.Second
+	config.PollInterval = time.Duration(pollIntervalSec) * time.Second
 
 	if !strings.HasPrefix(config.ServerAddress, "http://") {
 		config.ServerAddress = "http://" + config.ServerAddress
